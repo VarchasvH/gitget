27393ad330a4e85fc5b70652dc46b7c00a0de978
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-04-27 17:37:35 +0530

    Typescript error removal

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -5,6 +5,26 @@ import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import Image from "next/image";
+interface UserData {
+  name: string;
+  login: string;
+  avatar_url: string;
+  created_at: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+}
+
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+  language: string;
+  // Add any other properties you need from the GitHub API response
+}
+
+type UserRepos = Repository[];
 
 import {
   Card,
@@ -17,8 +37,8 @@ import {
 
 const Profile = () => {
   const { username } = useParams();
-  const [userData, setUserData] = useState(null);
-  const [userRepos, setUserRepos] = useState(null);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userRepos, setUserRepos] = useState<UserRepos | null>(null);
   const formatDate = (dateString: any) => {
     const date = new Date(dateString);
     const monthNames = [
@@ -115,9 +135,9 @@ const Profile = () => {
               </p>
             </div>
           </div>
-          {userRepos && userRepos?.length > 0 && (
+          {userRepos && userRepos.length > 0 && (
             <div className='grid md:grid-cols-3 grid-row-1 gap-5 m-10'>
-              {userRepos?.slice(0, 6).map((repo) => (
+              {userRepos?.slice(0, 6).map((repo: any) => (
                 <Link href={repo.html_url} key='repo.id'>
                   <Card className='w-[350px] md:min-h-[230px] min-h-[200px] bg-[#d5d9ed] shadow-xl'>
                     <CardHeader>
